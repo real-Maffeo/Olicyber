9d89e21b476bffc611edae447b9b91305bd1b7cb
M4ff30 <127723981+real-Maffeo@users.noreply.github.com>
2023-11-11 00:12:16 +0100

    t'ho cambiato il re.findall() con re.search() e ho rimosso qualche riga vuota
    OTTIMIZZAZIONE

--- import requests.py
+++ import requests.py
@@ -3,7 +3,5 @@ import re
 
 url = "http://roller.challs.olicyber.it/get_flag.php"
 
-
 r = requests.get(url, allow_redirects=False)
-
-print(re.findall(r'flag{.*}', r.text)[0])
+print(re.search(r'flag{.*}', r.text).group())
